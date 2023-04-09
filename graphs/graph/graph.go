3e9b6e7052f6ce77ba88db7e5e33c8f5d76908ce
Ivo Stoyanov <ivo@skyway-consulting.com>
2023-04-08 17:26:28 -0700

    BFS

--- graphs/graph/graph.go
+++ graphs/graph/graph.go
@@ -15,9 +15,9 @@ const (
 type Node struct {
 	Idx      int
 	Value    string
-	Parent   *Node
-	Distance uint
 	Color    string
+	Distance uint
+	Parent   *int
 }
 
 func NewNode(value string) *Node {
@@ -28,6 +28,8 @@ type Graph struct {
 	V     int     // Number of vertices
 	Nodes []Node  // Nodes
 	Adj   [][]int // adj list - for each vertex - indices of connected nodes
+	// store only node indices
+	Q []int
 }
 
 func NewGraph(numNodes int) *Graph {
@@ -52,16 +54,60 @@ func (g *Graph) AddEdge(from int, to int) {
 	g.Adj[from] = append(g.Adj[from], to)
 }
 
+func (g *Graph) init(start int) {
+	// init
+	g.Q = make([]int, 0)
+	for i := 0; i < len(g.Nodes); i++ {
+		n := &g.Nodes[i]
+		if i == start {
+			n.Distance = 0
+			n.Parent = nil
+			n.Color = Gray
+			// enqueue start node
+			g.Q = append(g.Q, i)
+			continue
+		}
+		n.Color = White
+		n.Distance = Infinity
+		n.Parent = nil
+	}
+}
+
+func (g *Graph) BFS(start int) {
+	g.init(start)
+	for len(g.Q) > 0 {
+		i := g.Q[0]
+		u := &g.Nodes[i]
+		g.Q = g.Q[1:]
+		for j := 0; j < len(g.Adj[i]); j++ {
+			v := &g.Nodes[g.Adj[i][j]]
+			if v.Color == White {
+				v.Color = Gray
+				v.Distance = u.Distance + 1
+				v.Parent = &u.Idx
+				// enqueue graph node
+				g.Q = append(g.Q, v.Idx)
+			}
+		}
+		u.Color = Black
+		//log.Printf("Graph:\n%v", g.String())
+	}
+}
+
 func (g *Graph) String() string {
 	var b strings.Builder
 	for i, node := range g.Nodes {
-		b.WriteString(fmt.Sprintf("%s -> ", node.Value))
+		b.WriteString(fmt.Sprintf("%s (%v) -> ", node.Value, node.Color))
 		for j := 0; j < len(g.Adj[i]); j++ {
 			if j > 0 {
 				b.WriteString(", ")
 			}
 			a := g.Adj[i][j]
-			b.WriteString(fmt.Sprintf("%s", g.Nodes[a].Value))
+			n := g.Nodes[a]
+			b.WriteString(fmt.Sprintf("%s (%v %v)", n.Value, n.Color, n.Distance))
+			if n.Parent != nil {
+				b.WriteString(fmt.Sprintf(" parent %v", *n.Parent))
+			}
 		}
 		b.WriteString("\n")
 	}
--- graphs/graph/graph_test.go
+++ graphs/graph/graph_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestGraph_Add(t *testing.T) {
+func TestBFS(t *testing.T) {
 	g := NewGraph(6)
 	g.AddNode(0, "A")
 	g.AddNode(1, "B")
@@ -13,12 +13,21 @@ func TestGraph_Add(t *testing.T) {
 	g.AddNode(3, "D")
 	g.AddNode(4, "E")
 	g.AddNode(5, "F")
+	// A -> B, D
 	g.AddEdge(0, 1)
 	g.AddEdge(0, 3)
+	// B -> E
 	g.AddEdge(1, 4)
-	g.AddEdge(2, 4)
-	g.AddEdge(2, 5)
+	// D -> B
 	g.AddEdge(3, 1)
+	// E -> C
+	g.AddEdge(4, 2)
+	// F -> C
+	g.AddEdge(5, 2)
 
-	log.Printf("Graph:\n%v", g.String())
+	log.Printf("Graph before BFS:\n%v", g.String())
+
+	g.BFS(0)
+
+	log.Printf("Graph after BFS:\n%v", g.String())
 }
