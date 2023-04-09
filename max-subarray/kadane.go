a5ddaeed9b3f5d8e8abca41d15ca2c87237e1d48
Ivo Stoyanov <ivo@skyway-consulting.com>
2023-04-09 14:56:42 -0700

    wip

--- max-subarray/kadane.go
+++ max-subarray/kadane.go
@@ -7,12 +7,20 @@ import (
 
 // kadane algorithm
 func maxSubarray(arr []int) int {
+	log.Printf("maxSubarray %v", arr)
 	maxEndingHere, maxSoFar := arr[0], arr[0]
-
 	for i := 1; i < len(arr); i++ {
-		maxEndingHere = max(arr[i], maxEndingHere+arr[i])
-		maxSoFar = max(maxSoFar, maxEndingHere)
-		log.Printf("i %v, maxEndingHere %v, maxSoFar %v", i, maxEndingHere, maxSoFar)
+		n := arr[i]
+		log.Printf("i %v, n %v", i, n)
+		log.Printf("maxEndingHere %v, maxSoFar %v", maxEndingHere, maxSoFar)
+		maxEndingHere = max(n, maxEndingHere+n)
+		//maxSoFar = max(maxSoFar, maxEndingHere)
+		if maxSoFar < maxEndingHere {
+			maxSoFar = maxEndingHere
+			log.Printf(">>> i %v, n %v", i, n)
+			log.Printf(">>> maxEndingHere %v, maxSoFar %v", maxEndingHere, maxSoFar)
+		}
+		//log.Printf("maxEndingHere %v, maxSoFar %v", maxEndingHere, maxSoFar)
 	}
 
 	return maxSoFar
@@ -26,9 +34,9 @@ func max(a, b int) int {
 }
 
 func main() {
-	//arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	//p := []int{23, 13, 25, 29, 33, 19, 34, 45, 65, 67}
-	arr := []int{0, -10, 12, 4, 16, -14, 15, 11, 20}
+	//arr := []int{0, -10, 12, 4, 16, -14, 15, 11, 20}
 
 	maxSum := maxSubarray(arr)
 	fmt.Println("Maximum subarray sum:", maxSum)
