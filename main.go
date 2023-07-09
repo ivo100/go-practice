cad7f7d6657f9c8837c1f4913f6885660d6908eb
Ivo Stoyanov <ivostoy@me.com>
2023-07-09 09:18:55 -0700

    iota

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ivo100/go-practice/cache"
 	userservice "github.com/ivo100/go-practice/userservice/pkg"
 	"log"
@@ -13,15 +14,35 @@ type Svc struct {
 	userservice.UserService
 }
 
+type TimeZone int
+
+const (
+	EST TimeZone = -(5 + iota)
+	CST
+	MST
+	PST
+)
+
+func (tz TimeZone) String() string {
+	return fmt.Sprintf("UTC%+d", tz)
+}
+
 func main() {
-	cycle := 100 * time.Millisecond
-	c := cache.New(cycle)
+	duration := 100 * time.Millisecond
+	c := cache.New(duration)
 	defer c.Close()
 
+	log.Printf("EST %v, PST %v", EST, PST)
+
 	c.Set("sticky", "forever", 0)
-	c.Set("hello", "Hello", cycle/2)
+	c.Set("hello", "Hello", duration/2)
 	hello, found := c.Get("hello")
-	log.Printf("found %v, hello %v", found, hello)
+	log.Printf("cache TTL %v", duration)
+	log.Printf("found %v, value %v", found, hello)
+	time.Sleep(duration)
+	hello, found = c.Get("hello")
+	log.Printf("must expire - found %v, value %v", found, hello)
+
 	var svc Svc
 	_ = svc
 }
