cea3ff4542bc32e8d095edbdf92dc9233d9e8d70
ivo100 <ivostoy@gmail.com>
2024-03-17 18:42:55 -0700

    nats

--- sandbox/go.mod
+++ sandbox/go.mod
@@ -6,19 +6,24 @@ require (
 	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73
 	github.com/go-gota/gota v0.12.0
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/nats-io/nats.go v1.33.1
 	github.com/rivo/tview v0.0.0-20240120095012-7568c3f71cfa
 	github.com/traefik/yaegi v0.15.1
 )
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/nats-io/nkeys v0.4.7 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/net v0.6.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gonum.org/v1/gonum v0.9.1 // indirect
 )
---
+++ sandbox/hello_nats/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"time"
+)
+
+func main() {
+	// Connect to a server
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		panic(err)
+	}
+
+	// Simple Async Subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+	})
+
+	// Simple Publisher
+	err = nc.Publish("foo", []byte("Hello 111"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Responding to a request message
+	nc.Subscribe("request", func(m *nats.Msg) {
+		m.Respond([]byte("answer is 42"))
+	})
+
+	// Simple Sync Subscriber
+	sub, err := nc.SubscribeSync("foo")
+	timeout := time.Second
+	m, err := sub.NextMsg(timeout)
+	if err == nil {
+		fmt.Printf("Received: %s\n", string(m.Data))
+	}
+
+	// Channel Subscriber
+	ch := make(chan *nats.Msg, 64)
+	sub, err = nc.ChanSubscribe("foo", ch)
+	if err != nil {
+		panic(err)
+	}
+
+	err = nc.Publish("foo", []byte("Hello 2222"))
+	if err != nil {
+		panic(err)
+	}
+
+	msg := <-ch
+	fmt.Printf("Received: %s\n", string(msg.Data))
+	// Unsubscribe
+	sub.Unsubscribe()
+
+	// Drain
+	sub.Drain()
+
+	// Replies
+	// subscribe must be before publish
+	nc.Subscribe("help", func(m *nats.Msg) {
+		nc.Publish(m.Reply, []byte("I can help!"))
+	})
+
+	// Requests
+	msg, err = nc.Request("help", []byte("help me"), 10*time.Millisecond)
+	if msg != nil {
+		fmt.Printf("Received: %s\n", string(msg.Data))
+	}
+
+	// Drain connection (Preferred for responders)
+	// Close() not needed if this is called.
+	nc.Drain()
+
+	//time.Sleep(2 * time.Second)
+	// Close connection
+	nc.Close()
+}
