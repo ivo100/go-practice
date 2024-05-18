75fbb76517758aad2bac43232c92b9606800a61b
Ivo Stoyanov <ivostoy@me.com>
2024-05-18 08:58:58 -0700

    wip

---
+++ babyapi/todo-htmx/README.md
@@ -0,0 +1,13 @@
+# TODO Example Application
+
+<img alt="sorry darkmode users" src="https://raw.githubusercontent.com/calvinmclean/calvinmclean.github.io/main/articles/babyapi-htmx/ui-demo.gif"/>
+
+This is an example that uses `babyapi` to create a TODO list application with an API and HTMX frontend.
+
+Run the application:
+
+```shell
+go run main.go
+```
+
+Then, use the UI at <http://localhost:8080/todos>
---
+++ babyapi/todo/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/calvinmclean/babyapi"
+)
+
+type TODO struct {
+	babyapi.DefaultResource
+
+	Title       string
+	Description string
+	Completed   *bool
+	CreatedAt   time.Time
+}
+
+func (t *TODO) Patch(newTODO *TODO) *babyapi.ErrResponse {
+	if newTODO.Title != "" {
+		t.Title = newTODO.Title
+	}
+	if newTODO.Description != "" {
+		t.Description = newTODO.Description
+	}
+	if newTODO.Completed != nil {
+		t.Completed = newTODO.Completed
+	}
+
+	return nil
+}
+
+func (t *TODO) Bind(r *http.Request) error {
+	err := t.DefaultResource.Bind(r)
+	if err != nil {
+		return err
+	}
+
+	switch r.Method {
+	case http.MethodPost:
+		t.CreatedAt = time.Now()
+		fallthrough
+	case http.MethodPut:
+		if t.Title == "" {
+			return errors.New("missing required title field")
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	api := babyapi.NewAPI("TODOs", "/todos", func() *TODO { return &TODO{} })
+	api.SetGetAllFilter(func(r *http.Request) babyapi.FilterFunc[*TODO] {
+		return func(t *TODO) bool {
+			getCompletedParam := r.URL.Query().Get("completed")
+			// No filtering if param is not provided
+			if getCompletedParam == "" {
+				return true
+			}
+
+			if getCompletedParam == "true" {
+				return t.Completed != nil && *t.Completed
+			}
+
+			return t.Completed == nil || !*t.Completed
+		}
+	})
+
+	api.RunCLI()
+}
